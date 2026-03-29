3730a784ee20ab61e3cd0afbf7eb94aeffb2b1a7
M-zz-k <mizbakhanun2006@gmail.com>
2026-03-29 16:45:46 +0530

    Model Saved and updated

--- House_pred.py
+++ House_pred.py
@@ -1,40 +1,13 @@
-# import pandas as pd
-# df =pd.read_csv("data.csv")
-
-# df = df.drop(['street','country'],axis = 1)
-# df = df.drop(['statezip'], axis=1)
-# df['date'] = pd.to_datetime(df['date'])
-# df['year'] = df['date'].dt.year
-# df['month'] = df['date'].dt.month
-# df = df.drop('date',axis = 1)
-
-# df = pd.get_dummies(df,drop_first = True)
-
-# X = df.drop("price",axis = 1)
-# y = df['price']
-
-# from sklearn.model_selection import train_test_split
-# x_train ,x_test, y_train, y_test = train_test_split(X,y,random_state=42,test_size=0.2)
-
-# # from sklearn.linear_model import LinearRegression
-# from sklearn.ensemble import RandomForestRegressor
-# model = RandomForestRegressor()
-# model.fit(x_train,y_train)
-# pred = model.predict(x_test)
-
-# from sklearn.metrics import r2_score,mean_squared_error
-# import math
-# r2 = r2_score(y_test,pred)
-# rms = math.sqrt(mean_squared_error(y_test,pred))
-# print("R2: ",r2)
-# print("RMS: ",rms)
-
 import pandas as pd
 import numpy as np
+import pickle 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score,mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
 
-df = pd.read_csv("data.csv")
+df = pd.read_csv("data/data.csv")
 
-# Drop useless
+# Drop useless faetures
 df = df.drop(['street','country'], axis=1)
 
 # Handle date
@@ -60,22 +33,21 @@ df = pd.get_dummies(df, drop_first=True)
 # Split
 X = df.drop("price", axis=1)
 y = df["price"]
-
-from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Model
-from sklearn.ensemble import RandomForestRegressor
 model = RandomForestRegressor(n_estimators=200)
 model.fit(x_train, y_train)
 
 # Predict
 pred = model.predict(x_test)
 
-from sklearn.metrics import r2_score, mean_squared_error
+#Testing model
+# r2 = r2_score(y_test, pred)
+# rmse = np.sqrt(mean_squared_error(y_test, pred))
+# print("R2:", r2)
+# print("RMSE:", rmse)
 
-r2 = r2_score(y_test, pred)
-rmse = np.sqrt(mean_squared_error(y_test, pred))
-
-print("R2:", r2)
-print("RMSE:", rmse)
+pickle.dump(model,open("model.pkl","wb"))
+pickle.dump(X.columns,open("columns.pkl","wb"))
+print("Model trained successfully.")
